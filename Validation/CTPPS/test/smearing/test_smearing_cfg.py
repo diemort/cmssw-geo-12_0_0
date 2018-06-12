959fb6b34976b138932e5c05fef9e6c9ded5371d
Jan Kaspar <jan.kaspar@cern.ch>
2018-06-12 12:58:38 +0200

    RandomXiThetaGunProducer used.

--- Validation/CTPPS/test/smearing/test_smearing_cfg.py
+++ Validation/CTPPS/test/smearing/test_smearing_cfg.py
@@ -23,24 +23,19 @@ process.maxEvents = cms.untracked.PSet(
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
 # particle generator
-process.generator = cms.EDProducer("RandomtXiGunProducer",
-  Verbosity = cms.untracked.int32(0),
+process.generator = cms.EDProducer("RandomXiThetaGunProducer",
+  particleId = cms.uint32(2212),
 
-  FireBackward = cms.bool(True),
-  FireForward = cms.bool(True),
+  energy = cms.double(6500),  # nominal beam energy, GeV
+  xi_min = cms.double(0.),
+  xi_max = cms.double(0.20),
+  theta_x_mean = cms.double(0),
+  theta_x_sigma = cms.double(100E-6), # in rad
+  theta_y_mean = cms.double(0),
+  theta_y_sigma = cms.double(100E-6),
 
-  PGunParameters = cms.PSet(
-    PartID = cms.vint32(2212),
-    ECMS = cms.double(13E3),
-
-    Mint = cms.double(0),
-    Maxt = cms.double(3),
-    MinXi = cms.double(0.0),
-    MaxXi = cms.double(0.2),
-
-    MinPhi = cms.double(-3.14159265359),
-    MaxPhi = cms.double(+3.14159265359)
-  )
+  nParticlesSector45 = cms.uint32(1),
+  nParticlesSector56 = cms.uint32(1),
 )
 
 # random seeds
