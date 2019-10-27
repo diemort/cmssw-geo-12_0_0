27e4c6cf2c605cf500c763d5920e4f681431b88b
Jan Kašpar <jan.kaspar@cern.ch>
2019-10-27 13:08:25 +0100

    Added more optical functions and opticsLabel parameter.

--- Validation/CTPPS/plugins/CTPPSOpticsPlotter.cc
+++ Validation/CTPPS/plugins/CTPPSOpticsPlotter.cc
@@ -33,17 +33,42 @@ class CTPPSOpticsPlotter : public edm::one::EDAnalyzer<>
     void analyze(const edm::Event&, const edm::EventSetup&) override;
     void endJob() override;
 
+    std::string opticsLabel_;
+
     std::string outputFile_;
 
     struct RPPlots
     {
+      std::unique_ptr<TGraph> g_v_x_vs_xi, g_L_x_vs_xi, g_x_D_vs_xi;
+      std::unique_ptr<TGraph> g_v_y_vs_xi, g_L_y_vs_xi, g_y_D_vs_xi;
       std::unique_ptr<TGraph> h_y_vs_x_disp;
+
       RPPlots() :
+        g_v_x_vs_xi(new TGraph), g_L_x_vs_xi(new TGraph), g_x_D_vs_xi(new TGraph),
+        g_v_y_vs_xi(new TGraph), g_L_y_vs_xi(new TGraph), g_y_D_vs_xi(new TGraph),
         h_y_vs_x_disp(new TGraph)
       {}
 
       void write() const {
-        h_y_vs_x_disp->SetTitle(";x   (mm);y   (mm)");
+        g_v_x_vs_xi->SetTitle(";xi;v_{x}");
+        g_v_x_vs_xi->Write("g_v_x_vs_xi");
+
+        g_L_x_vs_xi->SetTitle(";xi;L_{x}   (cm)");
+        g_L_x_vs_xi->Write("g_L_x_vs_xi");
+
+        g_x_D_vs_xi->SetTitle(";xi;x_{D}   (cm)");
+        g_x_D_vs_xi->Write("g_x_D_vs_xi");
+
+        g_v_y_vs_xi->SetTitle(";xi;v_{y}");
+        g_v_y_vs_xi->Write("g_v_y_vs_xi");
+
+        g_L_y_vs_xi->SetTitle(";xi;L_{y}   (cm)");
+        g_L_y_vs_xi->Write("g_L_y_vs_xi");
+
+        g_y_D_vs_xi->SetTitle(";xi;y_{D}   (cm)");
+        g_y_D_vs_xi->Write("g_y_D_vs_xi");
+
+        h_y_vs_x_disp->SetTitle(";x   (cm);y   (cm)");
         h_y_vs_x_disp->Write("h_y_vs_x_disp");
       }
     };
@@ -59,6 +84,7 @@ using namespace edm;
 //----------------------------------------------------------------------------------------------------
 
 CTPPSOpticsPlotter::CTPPSOpticsPlotter(const edm::ParameterSet& iConfig) :
+  opticsLabel_(iConfig.getParameter<std::string>("opticsLabel")),
   outputFile_(iConfig.getParameter<string>("outputFile"))
 {}
 
@@ -72,7 +98,7 @@ void CTPPSOpticsPlotter::analyze(const edm::Event& iEvent, const edm::EventSetup
 
   // get conditions
   edm::ESHandle<LHCInterpolatedOpticalFunctionsSetCollection> hOpticalFunctions;
-  iSetup.get<CTPPSInterpolatedOpticsRcd>().get(hOpticalFunctions);
+  iSetup.get<CTPPSInterpolatedOpticsRcd>().get(opticsLabel_, hOpticalFunctions);
 
   // stop if conditions invalid
   if (hOpticalFunctions->empty())
@@ -90,17 +116,42 @@ void CTPPSOpticsPlotter::analyze(const edm::Event& iEvent, const edm::EventSetup
     LHCInterpolatedOpticalFunctionsSet::Kinematics k_out_beam;
     it.second.transport(k_in_beam, k_out_beam);
 
+    const double vtx_ep = 1E-4; // cm
+    const double th_ep = 1E-6; // rad
+
     for (double xi = 0.; xi < 0.30001; xi += 0.001)
     {
-      LHCInterpolatedOpticalFunctionsSet::Kinematics k_in = { 0., 0., 0., 0., xi };  // conversions: CMS --> LHC convention
-      LHCInterpolatedOpticalFunctionsSet::Kinematics k_out;
-      it.second.transport(k_in, k_out);
+      LHCInterpolatedOpticalFunctionsSet::Kinematics k_in_xi = { 0., 0., 0., 0., xi };
+      LHCInterpolatedOpticalFunctionsSet::Kinematics k_out_xi;
+      it.second.transport(k_in_xi, k_out_xi);
 
-      const double x = (k_out.x - k_out_beam.x) * 10.;  // conversions: cm --> mm
-      const double y = (k_out.y - k_out_beam.y) * 10.;
+      LHCInterpolatedOpticalFunctionsSet::Kinematics k_in_xi_vtx_x = { vtx_ep, 0., 0., 0., xi };
+      LHCInterpolatedOpticalFunctionsSet::Kinematics k_out_xi_vtx_x;
+      it.second.transport(k_in_xi_vtx_x, k_out_xi_vtx_x);
 
-      int idx = pl.h_y_vs_x_disp->GetN();
-      pl.h_y_vs_x_disp->SetPoint(idx, x, y);
+      LHCInterpolatedOpticalFunctionsSet::Kinematics k_in_xi_th_x = { 0., th_ep, 0., 0., xi };
+      LHCInterpolatedOpticalFunctionsSet::Kinematics k_out_xi_th_x;
+      it.second.transport(k_in_xi_th_x, k_out_xi_th_x);
+
+      LHCInterpolatedOpticalFunctionsSet::Kinematics k_in_xi_vtx_y = { 0., 0., vtx_ep, 0., xi };
+      LHCInterpolatedOpticalFunctionsSet::Kinematics k_out_xi_vtx_y;
+      it.second.transport(k_in_xi_vtx_y, k_out_xi_vtx_y);
+
+      LHCInterpolatedOpticalFunctionsSet::Kinematics k_in_xi_th_y = { 0., 0., 0., th_ep, xi };
+      LHCInterpolatedOpticalFunctionsSet::Kinematics k_out_xi_th_y;
+      it.second.transport(k_in_xi_th_y, k_out_xi_th_y);
+
+      int idx = pl.g_v_x_vs_xi->GetN();
+
+      pl.g_v_x_vs_xi->SetPoint(idx, xi, (k_out_xi_vtx_x.x - k_out_xi.x) / vtx_ep);
+      pl.g_L_x_vs_xi->SetPoint(idx, xi, (k_out_xi_th_x.x - k_out_xi.x) / th_ep);
+      pl.g_x_D_vs_xi->SetPoint(idx, xi, k_out_xi.x - k_out_beam.x);
+
+      pl.g_v_y_vs_xi->SetPoint(idx, xi, (k_out_xi_vtx_y.y - k_out_xi.y) / vtx_ep);
+      pl.g_L_y_vs_xi->SetPoint(idx, xi, (k_out_xi_th_y.y - k_out_xi.y) / th_ep);
+      pl.g_y_D_vs_xi->SetPoint(idx, xi, k_out_xi.y - k_out_beam.y);
+
+      pl.h_y_vs_x_disp->SetPoint(idx, k_out_xi.x - k_out_beam.x, k_out_xi.y - k_out_beam.y);
     }
   }
 }
