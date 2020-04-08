fe63f87bf250176314f9ae3d43bbea4c6577ed8a
Jan Kaspar <jan.kaspar@cern.ch>
2020-04-08 11:08:22 +0200

    Added example code to use test optics.

--- CalibPPS/ESProducers/python/ctppsOpticalFunctions_cff.py
+++ CalibPPS/ESProducers/python/ctppsOpticalFunctions_cff.py
@@ -4,26 +4,31 @@ from CalibPPS.ESProducers.ctppsLHCInfo_cff import *
 
 # by default, (raw) optical functions are now loaded from CondDB using a GT
 
-#from CalibPPS.ESProducers.ctppsOpticalFunctionsESSource_cfi import *
-#
-## add 2016 pre-TS2 configuration
-#config_2016_preTS2 = cms.PSet(
-#  validityRange = cms.EventRange("273725:min - 280385:max"),
-#
-#  opticalFunctions = cms.VPSet(
-#      cms.PSet( xangle = cms.double(185), fileName = cms.FileInPath("CalibPPS/ESProducers/data/optical_functions_2016.root") )
-#  ),
-#
-#  scoringPlanes = cms.VPSet(
-#      # z in cm
-#      cms.PSet( rpId = cms.uint32(0x76100000), dirName = cms.string("XRPH_C6L5_B2"), z = cms.double(-20382.6) ),  # RP 002, strip
-#      cms.PSet( rpId = cms.uint32(0x76180000), dirName = cms.string("XRPH_D6L5_B2"), z = cms.double(-21255.1) ),  # RP 003, strip
-#      cms.PSet( rpId = cms.uint32(0x77100000), dirName = cms.string("XRPH_C6R5_B1"), z = cms.double(+20382.6) ),  # RP 102, strip
-#      cms.PSet( rpId = cms.uint32(0x77180000), dirName = cms.string("XRPH_D6R5_B1"), z = cms.double(+21255.1) ),  # RP 103, strip
-#  )
-#)
-#
-#ctppsOpticalFunctionsESSource.configuration.append(config_2016_preTS2)
+# TODO: remove
+
+###   from CalibPPS.ESProducers.ctppsOpticalFunctionsESSource_cfi import *
+###
+###   config_2018 = cms.PSet(
+###     validityRange = cms.EventRange("0:min - 999999:max"),
+###
+###     opticalFunctions = cms.VPSet(
+###         cms.PSet( xangle = cms.double(120), fileName = cms.FileInPath("CalibPPS/ESProducers/data/optical_functions/2018/version5-test/120urad.root") ),
+###         cms.PSet( xangle = cms.double(130), fileName = cms.FileInPath("CalibPPS/ESProducers/data/optical_functions/2018/version5-test/130urad.root") ),
+###         cms.PSet( xangle = cms.double(140), fileName = cms.FileInPath("CalibPPS/ESProducers/data/optical_functions/2018/version5-test/140urad.root") )
+###     ),
+###
+###     scoringPlanes = cms.VPSet(
+###         # z in cm
+###         cms.PSet( rpId = cms.uint32(2014838784), dirName = cms.string("XRPH_D6L5_B2"), z = cms.double(-21255.1) ),  # RP 003, pixel
+###         cms.PSet( rpId = cms.uint32(2023227392), dirName = cms.string("XRPH_B6L5_B2"), z = cms.double(-21955.0) ),  # RP 023, pixel
+###         cms.PSet( rpId = cms.uint32(2031616000), dirName = cms.string("XRPH_D6R5_B1"), z = cms.double(+21255.1) ),  # RP 103, pixel
+###         cms.PSet( rpId = cms.uint32(2040004608), dirName = cms.string("XRPH_B6R5_B1"), z = cms.double(+21955.0) ),  # RP 123, pixel
+###     )
+###   )
+###
+###   ctppsOpticalFunctionsESSource.configuration.append(config_2018)
+###
+###   esPreferLocalOptics = cms.ESPrefer("CTPPSOpticalFunctionsESSource", "ctppsOpticalFunctionsESSource")
 
 # optics interpolation between crossing angles
 from CalibPPS.ESProducers.ctppsInterpolatedOpticalFunctionsESSource_cfi import *
