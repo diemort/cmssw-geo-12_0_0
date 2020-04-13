1ccd93368af04f7e1da2dcfff1fe7468fa0e45d7
Jan Kaspar <jan.kaspar@cern.ch>
2020-04-13 14:35:25 +0200

    Commented out local alignment.

--- CalibPPS/ESProducers/python/ctppsAlignment_cff.py
+++ CalibPPS/ESProducers/python/ctppsAlignment_cff.py
@@ -4,32 +4,33 @@ import FWCore.ParameterSet.Config as cms
 
 # TODO: comment out
 
-from CalibPPS.ESProducers.ctppsRPAlignmentCorrectionsDataESSourceXML_cfi import *
-ctppsRPAlignmentCorrectionsDataESSourceXML.RealFiles = cms.vstring(
-  # internal pixel alignment
-  #"CalibPPS/ESProducers/data/alignment/common/RPixGeometryCorrections_old.xml",
-  "CalibPPS/ESProducers/data/alignment/common/RPixGeometryCorrections-2017-2018.xml",
-
-  # 2016 pre-TS2
-  "CalibPPS/ESProducers/data/alignment/2016/tracking_RP_preTS2_version_10.xml",
-
-  # 2016 post-TS2
-  "CalibPPS/ESProducers/data/alignment/2016/tracking_RP_postTS2_version_10.xml",
-
-  # 2017 pre-TS2
-  "CalibPPS/ESProducers/data/alignment/2017/tracking_RP_preTS2_version_10.xml",
-
-  "CalibPPS/ESProducers/data/alignment/2017/timing_RP_preTS2_v2.xml",
-
-  # 2017 post-TS2
-  "CalibPPS/ESProducers/data/alignment/2017/tracking_RP_postTS2_version_10.xml",
-
-  "CalibPPS/ESProducers/data/alignment/2017/timing_RP_postTS2_v4.xml",
-
-  # 2018
-  "CalibPPS/ESProducers/data/alignment/2018/tracking_RP_version_10.xml",
-
-  "CalibPPS/ESProducers/data/alignment/2018/timing_RP_v4.xml"
-)
-
-esPreferLocalAlignment = cms.ESPrefer("CTPPSRPAlignmentCorrectionsDataESSourceXML", "ctppsRPAlignmentCorrectionsDataESSourceXML")
+###   from CalibPPS.ESProducers.ctppsRPAlignmentCorrectionsDataESSourceXML_cfi import *
+###   ctppsRPAlignmentCorrectionsDataESSourceXML.RealFiles = cms.vstring(
+###     # internal pixel alignment
+###     #"CalibPPS/ESProducers/data/alignment/common/RPixGeometryCorrections_old.xml",
+###     "CalibPPS/ESProducers/data/alignment/common/RPixGeometryCorrections-2017-2018.xml",
+###   
+###     # 2016 pre-TS2
+###     "CalibPPS/ESProducers/data/alignment/2016/tracking_RP_preTS2_version_10.xml",
+###   
+###     # 2016 post-TS2
+###     "CalibPPS/ESProducers/data/alignment/2016/tracking_RP_postTS2_version_10.xml",
+###   
+###     # 2017 pre-TS2
+###     "CalibPPS/ESProducers/data/alignment/2017/tracking_RP_preTS2_version_10.xml",
+###   
+###     "CalibPPS/ESProducers/data/alignment/2017/timing_RP_preTS2_v2.xml",
+###   
+###     # 2017 post-TS2
+###     "CalibPPS/ESProducers/data/alignment/2017/tracking_RP_postTS2_version_10.xml",
+###   
+###     "CalibPPS/ESProducers/data/alignment/2017/timing_RP_postTS2_v4.xml",
+###   
+###     # 2018
+###     #"CalibPPS/ESProducers/data/alignment/2018/tracking_RP_version_10.xml",
+###     "CalibPPS/ESProducers/data/alignment/2018/tracking_RP_version_11_test1.xml",
+###   
+###     "CalibPPS/ESProducers/data/alignment/2018/timing_RP_v4.xml"
+###   )
+###   
+###   esPreferLocalAlignment = cms.ESPrefer("CTPPSRPAlignmentCorrectionsDataESSourceXML", "ctppsRPAlignmentCorrectionsDataESSourceXML")
