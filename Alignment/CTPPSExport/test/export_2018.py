1443fc49bd4e17d0df5aa0437968a7d9b9319aa2
Jan Kaspar <jan.kaspar@cern.ch>
2020-04-15 19:13:53 +0200

    Test 2018 alignment.

--- Alignment/CTPPSExport/test/export_2018.py
+++ Alignment/CTPPSExport/test/export_2018.py
@@ -7,4 +7,5 @@ process.load("Geometry.VeryForwardGeometry.geometryRPFromDD_2018_cfi")
 # configure
 from base_cff import *
 
-Initialize(process, "Alignment/CTPPSExport/data/2018/fit_alignments_2019_07_11.out", "tracking_RP_2018_version_10.xml")
+#Initialize(process, "Alignment/CTPPSExport/data/2018/fit_alignments_2019_07_11.out", "tracking_RP_2018_version_10.xml")
+Initialize(process, "Alignment/CTPPSExport/data/2018/fit_alignments_2020_04_10.out", "tracking_RP_2018_version_11_test1.xml")
